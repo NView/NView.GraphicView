9d8810b98b826e492f716aec2dd8a159692fbc86
Frank A. Krueger <fak@praeclarum.org>
2015-04-12 16:59:56 -0700

    Initial commit

---
+++ NView.GraphicView.Mac/GraphicView.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+using AppKit;
+
+using NGraphics;
+
+namespace NView.GraphicView
+{
+	public class GraphicView : IView
+	{
+		public virtual void Draw (ICanvas canvas, Rect dirtyRect)
+		{
+		}
+
+		#region IView implementation
+		public IDisposable BindToNative (object nativeView)
+		{
+			var view = PlatformHelpers.GetView<NativeGraphicView> (nativeView);
+			view.GraphicView = this;
+			return PlatformHelpers.CreateDisposable (() => {
+				if (view.GraphicView == this)
+					view.GraphicView = null;
+			});
+		}
+		public Type PreferredNativeType {
+			get {
+				return typeof(NativeGraphicView);
+			}
+		}
+		#endregion
+	}
+
+	public class NativeGraphicView : NSView
+	{
+		public GraphicView GraphicView;
+
+		public override void DrawRect (CoreGraphics.CGRect dirtyRect)
+		{
+			var c = new CGContextCanvas (NSGraphicsContext.CurrentContext.CGContext);
+			if (GraphicView != null) {
+				GraphicView.Draw (c, Conversions.GetRect (dirtyRect));
+			}
+		}
+	}
+}
+
---
+++ NView.GraphicView/GraphicView.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using NGraphics;
+
+namespace NView.GraphicView
+{
+	public class GraphicView : IView
+	{
+		public virtual void Draw (ICanvas canvas, Rect dirtyRect)
+		{
+		}
+
+		#region IView implementation
+		public IDisposable BindToNative (object nativeView)
+		{
+			throw new NotImplementedException ();
+		}
+		public Type PreferredNativeType {
+			get {
+				throw new NotImplementedException ();
+			}
+		}
+		#endregion		
+	}
+}
+
