973468abeec37514eac435f1535f4412c5fab34a
Frank A. Krueger <fak@praeclarum.org>
2015-04-12 18:53:33 -0700

    Added iOS and Android

---
+++ NView.GraphicView.Android/GraphicView.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+using Android.Views;
+
+using NGraphics;
+using Android.Content;
+
+namespace NView.GraphicView
+{
+	public class GraphicView : IView
+	{
+		public virtual void Draw (ICanvas canvas, Rect dirtyRect)
+		{
+		}
+
+		#region IView implementation
+		public IDisposable BindToNative (object nativeView)
+		{
+			var view = ViewHelpers.GetView<NativeGraphicView> (nativeView);
+			view.GraphicView = this;
+			return new DisposeAction (() => {
+				if (view.GraphicView == this)
+					view.GraphicView = null;
+			});
+		}
+		public Type PreferredNativeType {
+			get {
+				return typeof(NativeGraphicView);
+			}
+		}
+		#endregion
+	}
+
+	public class NativeGraphicView : View
+	{
+		public NativeGraphicView (Context c)
+			: base (c)
+		{			
+		}
+
+		public GraphicView GraphicView;
+		public override void Draw (global::Android.Graphics.Canvas canvas)
+		{
+			var c = new CanvasCanvas (canvas);
+			if (GraphicView != null) {
+				var r = new global::Android.Graphics.Rect ();
+				canvas.GetClipBounds (r);
+				GraphicView.Draw (c, new Rect (r.Left, r.Top, r.Width (), r.Height ()));
+			}
+		}
+	}
+}
+
---
+++ NView.GraphicView.iOS/GraphicView.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+using UIKit;
+
+using NGraphics;
+
+namespace NView.GraphicView
+{
+	public class GraphicView : IView
+	{
+		public virtual void Draw (ICanvas canvas, Rect dirtyRect)
+		{
+		}
+
+		#region IView implementation
+		public IDisposable BindToNative (object nativeView)
+		{
+			var view = ViewHelpers.GetView<NativeGraphicView> (nativeView);
+			view.GraphicView = this;
+			return new DisposeAction (() => {
+				if (view.GraphicView == this)
+					view.GraphicView = null;
+			});
+		}
+		public Type PreferredNativeType {
+			get {
+				return typeof(NativeGraphicView);
+			}
+		}
+		#endregion
+	}
+
+	public class NativeGraphicView : UIView
+	{
+		public GraphicView GraphicView;
+
+		public override void Draw (CoreGraphics.CGRect dirtyRect)
+		{
+			var c = new CGContextCanvas (UIGraphics.GetCurrentContext ());
+			if (GraphicView != null) {
+				GraphicView.Draw (c, Conversions.GetRect (dirtyRect));
+			}
+		}
+	}
+}
+
